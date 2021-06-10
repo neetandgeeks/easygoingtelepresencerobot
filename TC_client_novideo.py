a1521e95f497ee702151986735dd64e2bcb1f2c3
neetandgeeks <85488234+neetandgeeks@users.noreply.github.com>
2021-06-10 16:17:09 +0900

    Add files via upload

---
+++ TC_client_novideo.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import time
+import datetime
+import paramiko
+import pygame
+import win32api
+import win32con
+import win32gui
+
+# IP Address of a SSH Server to connect
+#ip = "192.168.11.17"
+ip = "192.168.11.5"
+
+#Resolution
+resolution = 640,480
+#resolution = 320,240
+#resolution = 160,120
+
+# for shutdown
+POWER=""
+
+#Start PyGame:
+pygame.init()
+#screen = pygame.display.set_mode(resolution ,pygame.FULLSCREEN|pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.OPENGL|pygame.NOFRAME)
+screen = pygame.display.set_mode(resolution ,pygame.FULLSCREEN|pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.NOFRAME)
+#screen = pygame.display.set_mode(resolution)
+pygame.display.set_caption('Tele Command Client')
+clock = pygame.time.Clock()
+
+# Create layered window
+hwnd = pygame.display.get_wm_info()["window"]
+win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE,
+                        win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
+win32gui.SetLayeredWindowAttributes(hwnd, 0, 50, win32con.LWA_ALPHA)
+
+# Mouse
+#pygame.mouse.set_visible(False)
+
+# Joystick
+pygame.joystick.init()
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
+
+# Fonts
+pygame.font.init()
+font = pygame.font.SysFont("Arial",14)
+dialogue_font = pygame.font.SysFont('Arial', 15)
+alert_font = pygame.font.SysFont('notomono', 60)
+
+# Connect SSH
+client = paramiko.SSHClient()
+#client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.set_missing_host_key_policy(paramiko.WarningPolicy())
+print("Connect to the server.")
+client.connect(ip, username='pi', password='raspberry')
+
+#Main program loop:
+def main():
+    previousImage = ""
+    image = ""
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                client.close() # Disconnect SSH
+                sys.exit()
+            if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
+                if event.key == pygame.K_ESCAPE:
+                    global POWER
+                    POWER="OFF"
+                    sys.exit()
+            #Control Servo
+            if event.type == pygame.MOUSEWHEEL:
+                stdin, stdout, stderr = client.exec_command('ps ax | grep TC_server_novideo.py | grep --invert-match grep')
+                survive = stdout.channel.recv_exit_status()
+                if survive == 1:
+#                    pygame.display.quit()
+                    sys.exit()
+                motor_both0 = "echo "
+                motor_both1 = '"'
+                motor_both2 = " > motor_both_fifo"
+                motor_both = motor_both0 + motor_both1 + str(event.y) + motor_both1 + motor_both2
+                stdin, stdout, stderr = client.exec_command(motor_both)
+
+            if event.type == pygame.JOYAXISMOTION:
+                stdin, stdout, stderr = client.exec_command('ps ax | grep TC_server_novideo.py | grep --invert-match grep')
+                survive = stdout.channel.recv_exit_status()
+                if survive == 1:
+                    sys.exit()
+                raw_axis3 = joystick.get_axis(3)
+                str_axis3x200 = str(int(200 * raw_axis3))
+                servo_pan0 = "echo "
+                servo_pan1 = '"'
+                servo_pan2 = " > servo_pwm00_fifo"
+                servo_pan = servo_pan0 + servo_pan1 + str_axis3x200 + servo_pan1 + servo_pan2
+                stdin, stdout, stderr = client.exec_command(servo_pan)
+                raw_axis4 = joystick.get_axis(4)
+                str_axis4x100 = str(int(100 * raw_axis4))
+                servo_tilt0 = "echo "
+                servo_tilt1 = '"'
+                servo_tilt2 = " > servo_pwm01_fifo"
+                servo_tilt = servo_tilt0 + servo_tilt1 + str_axis4x100 + servo_tilt1 + servo_tilt2
+                stdin, stdout, stderr = client.exec_command(servo_tilt)
+                raw_axis0 = joystick.get_axis(0)
+                str_axis0x50 = str(int(50 * raw_axis0))
+                servo_steer0 = "echo "
+                servo_steer1 = '"'
+                servo_steer2 = " > servo_pwm02_fifo"
+                servo_steer = servo_steer0 + servo_steer1 + str_axis0x50 + servo_steer1 + servo_steer2
+                stdin, stdout, stderr = client.exec_command(servo_steer)
+
+        #Control Camera
+        #Receive data
+        stdin, stdout, stderr = client.exec_command('ps ax | grep TC_server_novideo.py | grep --invert-match grep')
+        survive = stdout.channel.recv_exit_status()
+        if survive == 1:
+            sys.exit()
+
+        # Define color
+        black = 0,0,0
+        white = 255,255,255
+        green = 0,255,0
+        # Render Background
+        screen.fill(black)
+
+        # Render messages
+
+        tracking = "Now Tracking your motion"
+        render_tracking = alert_font.render(tracking, True, green)
+        screen.blit(render_tracking, (10,10))
+
+        # Drop Shadow
+#        message00 = str(datetime.datetime.now())
+#        dialogue00 = dialogue_font.render(message00, True, black)
+#        screen.blit(dialogue00, (11,11))
+
+        # Render article
+#        dialogue01 = dialogue_font.render(message00, True, white)
+#        screen.blit(dialogue01, (10,10))
+
+        # Render axes values of joystick
+#        raw_axis3 = joystick.get_axis(3)
+#        message02 = str(int(200 * raw_axis3))
+#        dialogue02 = dialogue_font.render(message02, True, white)
+#        screen.blit(dialogue02, (10,30))
+
+        
+#        raw_axis4 = joystick.get_axis(4)
+#        message03 = str(int(100 * raw_axis4))
+#        dialogue03 = dialogue_font.render(message03, True, white)
+#        screen.blit(dialogue03, (10,60))
+        
+#        raw_axis0 = joystick.get_axis(0)
+#        message04 = str(int(50 * raw_axis0))
+#        dialogue04 = dialogue_font.render(message04, True, white)
+#        screen.blit(dialogue04, (10,90))
+
+#        clock.tick(120) # limit 120fps
+#        clock.tick(60) # limit 60fps
+        clock.tick(30)
+        pygame.display.flip()
+
+while True:
+    try:
+        print("Client is started.")
+        main()
+    except:
+        if POWER == "OFF":
+            pygame.display.quit()
+            client.close() # Disconnect SSH
+            print("Client is stopped.")
+            sys.exit()
+        print("Exception!")
+        print("Reconnect to the server.")
+        print(str(datetime.datetime.now()))
+        client.close()
+        client.connect(ip, username='pi', password='raspberry')
+        stdin, stdout, stderr = client.exec_command('ps ax | grep TC_server_novideo.py | grep --invert-match grep')
+        survive = stdout.channel.recv_exit_status()
+        if survive == 1:
+            black = 0,0,0
+            screen.fill(black)
+            print("Cannot reconnect to the server. Aborted.")
+            message041 = "Cannot connect"
+            message042 = "to the server!"
+            color04 = 255,0,0
+            dialogue041 = alert_font.render(message041, True, color04)
+            dialogue042 = alert_font.render(message042, True, color04)
+            screen.blit(dialogue041, (60,120))
+            screen.blit(dialogue042, (60,240))
+            clock.tick(1)
+            pygame.display.flip()
---
+++ TC_server.py
@@ -0,0 +1,167 @@
+# import modules
+import os
+import sys
+import time
+import threading
+# pygame
+import pygame
+import pygame.camera
+from pygame.locals import *
+# ezblock
+sys.path.append(r'/opt/ezblock')
+import picarmini
+# Servo
+from ezblock import Servo
+from ezblock import PWM
+from ezblock import delay
+
+# set variable
+# setup FIFO
+CAM_FIFO = 'cam_fifo'
+if(os.path.exists(CAM_FIFO)):
+    pass
+else:
+    os.mkfifo(CAM_FIFO)
+
+SERVO_PWM00_FIFO = 'servo_pwm00_fifo'
+if(os.path.exists(SERVO_PWM00_FIFO)):
+    pass
+else:
+    os.mkfifo(SERVO_PWM00_FIFO)
+
+SERVO_PWM01_FIFO = 'servo_pwm01_fifo'
+if(os.path.exists(SERVO_PWM01_FIFO)):
+    pass
+else:
+    os.mkfifo(SERVO_PWM01_FIFO)
+
+SERVO_PWM02_FIFO = 'servo_pwm02_fifo'
+if(os.path.exists(SERVO_PWM02_FIFO)):
+    pass
+else:
+    os.mkfifo(SERVO_PWM02_FIFO)
+
+MOTOR_BOTH_FIFO = 'motor_both_fifo'
+if(os.path.exists(MOTOR_BOTH_FIFO )):
+    pass
+else:
+    os.mkfifo(MOTOR_BOTH_FIFO )
+
+#resolution
+resolution = 640,480
+#resolution = 320,240
+#resolution = 160,120
+
+def camserver():
+#Start Pygame
+    pygame.init()
+    pygame.camera.init()
+
+    cam = pygame.camera.Camera("/dev/video0",resolution,"RGB")
+    cam.start()
+
+#Send data
+    while True:
+        data = cam.get_raw()
+#        image = cam.get_image()
+#        data = pygame.image.tostring(image,"RGB",False)
+        campipe = open(CAM_FIFO, 'wb')
+        campipe.write(data)
+        campipe.flush()
+        campipe.close()
+        time.sleep(0.008) #max 120fps (1/120=0.008333......)
+#        time.sleep(0.04) #max 25fps (1/25=0.04)
+#        time.sleep(0.1) #max 10fps (1/10=0.1)
+
+#Interupt
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+def servoserver_pan():
+    pwm_P0 = PWM("P0")
+    while True:
+        with open(SERVO_PWM00_FIFO) as pan:
+            for line in pan:
+                Servo(pwm_P0).angle(int(line))
+                delay(100) # set delay of a servo (ms)
+
+def servoserver_tilt():
+    pwm_P1 = PWM("P1")
+    while True:
+        with open(SERVO_PWM01_FIFO) as tilt:
+            for line in tilt:
+                Servo(pwm_P1).angle(int(line))
+                delay(100) # set delay of a servo (ms)
+
+def servoserver_steer():
+    pwm_P2 = PWM("P2")
+    while True:
+        with open(SERVO_PWM02_FIFO) as steer:
+            for line in steer:
+                Servo(pwm_P2).angle(int(line))
+                delay(100) # set delay of a servo (ms)
+
+def motorserver_both():
+    count = int(0)
+    while True:
+        with open(MOTOR_BOTH_FIFO) as motor:
+            zero = int(0)
+            for line in motor:
+                line = int(line) * int(100)
+#                print(line)
+# stop
+                if  line == zero:
+                    print("stop1")
+                    picarmini.forward(zero)
+# forward
+                if line > count:
+                    if count >= zero:
+                        print("forward1")
+                        picarmini.forward(line)
+                    if count < zero:
+                        if line > zero:
+                            print("stop2")
+                            picarmini.forward(zero)
+                if line == count:
+                    if count >= zero:
+                        print("forward2")
+                        picarmini.forward(line)
+                if line < count:
+                    if line > zero:
+                        print("forward3")
+                        picarmini.forward(line)
+# backward
+                if line < count:
+                    if count <= zero:
+                        print("backward1")
+                        picarmini.forward(line)
+                    if count > zero:
+                        if line < zero:
+                            print("stop3")
+                            picarmini.forward(zero)
+                if line == count:
+                    if count <= zero:
+                        print("backward2")
+                        picarmini.forward(line)
+                if line > count:
+                    if line < zero:
+                        print("backward3")
+                        picarmini.forward(line)
+# count
+                count = line
+#                print(count)
+
+t1 = threading.Thread(target=camserver)
+t2 = threading.Thread(target=servoserver_pan)
+t3 = threading.Thread(target=servoserver_tilt)
+t4 = threading.Thread(target=servoserver_steer)
+t5 = threading.Thread(target=motorserver_both)
+
+t1.start()
+t2.start()
+t3.start()
+t4.start()
+t5.start()
+
+print("Server is started.")
