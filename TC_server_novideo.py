519099ebb15fa00d5ff19266604bf10fcc2099bf
neetandgeeks <85488234+neetandgeeks@users.noreply.github.com>
2021-06-11 16:01:13 +0900

    Update TC_server_novideo.py

--- TC_server_novideo.py
+++ TC_server_novideo.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+# These codes are modified from the codes on the pages:
+# https://stackoverflow.com/questions/27676637/stream-video-in-python-use-pygame-lib
+
 # import modules
 import os
 import sys
